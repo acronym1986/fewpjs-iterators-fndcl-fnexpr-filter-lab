2ebd43637a583adfe00ab4a33b5bf3baa24c237d
Tim <acronym1986@gmail.com>
2021-04-07 18:25:36 +1000

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+// test 1
+function findMatching(arr, inputName) {
+      
+    let passedName = arr.filter (elemName => {
+      return elemName === inputName || elemName.includes(inputName.charAt(0).toLowerCase());
+       
+    })
+       return passedName;
+}
+
+
+//tesst 2
+function fuzzyMatch(arr, inputName){
+        let passedName = arr.filter (elemName => {
+            return  elemName.startsWith(inputName);
+        })
+        return passedName;
+}
+
+
+//test 3 
+
+function  matchName(arr, inputName) {
+  let getName = arr.filter( element => {
+       return element.name === inputName;
+  })
+ return getName;
+}
+matchName(drivers, 'Bobby')
